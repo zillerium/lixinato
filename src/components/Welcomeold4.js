3053ac2e392b22faad47ad8f8b11b974601eeaed
sales@merrows.co.uk <sales@merrows.co.uk>
2023-04-23 03:45:10 +0000

    add investments

--- src/components/Welcomeold4.js
+++ src/components/Welcomeold4.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { Row, Col } from 'react-bootstrap';
 
 const Welcome = () => {
   return (
     <div className="container">
-	    <Row className="mb-4">
+      <Row className="mb-4">
         <Col>
           <h1 className="text-center">Lixinato</h1>
           <p className="text-center lead">Blockchain solutions in Defi and decentralized ecommerce. Use the links below to find out more.</p>
@@ -13,40 +12,52 @@ const Welcome = () => {
       </Row>
       <Row className="justify-content-center">
         <Col xs={12} sm={6} md={6} lg={6} className="d-flex justify-content-center mb-4">
-          <Link to="/doramo">
+          <a href="https://example.com" target="_blank" rel="noopener noreferrer">
             <img
               src="doramoscreen.png"
               alt="Doramo"
               className="img-fluid w-100"
             />
-          </Link>
+          </a>
         </Col>
         <Col xs={12} sm={6} md={6} lg={6} className="d-flex justify-content-center mb-4">
-          <Link to="/flowswap">
+          <a href="https://example.com" target="_blank" rel="noopener noreferrer">
             <img
               src="fsscreen2.png"
               alt="Image 2"
               className="img-fluid w-100"
             />
-          </Link>
+          </a>
         </Col>
         <Col xs={12} sm={6} md={6} lg={6} className="d-flex justify-content-center mb-4">
-          <Link to="/catalox">
+          <a href="https://example.com" target="_blank" rel="noopener noreferrer">
             <img
               src="catalox2.png"
               alt="Image 3"
               className="img-fluid w-100"
             />
-          </Link>
+          </a>
         </Col>
         <Col xs={12} sm={6} md={6} lg={6} className="d-flex justify-content-center mb-4">
-          <Link to="/peacio">
+          <a href="https://example.com" target="_blank" rel="noopener noreferrer">
             <img
               src="peacioscreen.png"
               alt="Image 4"
               className="img-fluid w-100"
             />
-          </Link>
+          </a>
+        </Col>
+      </Row>
+      <Row className="justify-content-center">
+        <Col xs={12} md={6} className="mb-4">
+          <img
+            src="chainerascreen.png"
+            alt="Blockchain Consultancy"
+            className="img-fluid"
+          />
+        </Col>
+        <Col xs={12} md={6} className="mb-4">
+          <p><a href="https://chainera.network" target="_blank" rel="noopener noreferrer" className="text-white">Chainera</a> - Blockchain consultancy using solidity, web3, ipfs and more.</p>
         </Col>
       </Row>
     </div>
@@ -54,4 +65,3 @@ const Welcome = () => {
 };
 
 export default Welcome;
-
